c62e7effaa9828ad30cf5468d29d54e34e673151
Sai kiran <saikiran2707@hotmail.com>
2025-05-30 15:05:20 +0530

    commit

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { clerkWebhooks } from '../controllers/UserControllers.js'
-
-const userRouter = express.Router()
-
-userRouter.post('/webhooks',clerkWebhooks)
-
-export default userRouter
